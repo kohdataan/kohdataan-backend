fe771b533aa4e0f5f60dcd822dab22843f11ac93
Jonne Airaksinen <jonne.airaksinen@perfektio.fi>
2019-04-08 14:33:38 +0300

    #28 Initial docker setup

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Define default value for app container hostname and port
-APP_HOST=${APP_HOST:-mattermost}
-APP_PORT_NUMBER=${APP_PORT_NUMBER:-8000}
+MATTERMOST_HOST=${MATTERMOST_HOST:-mattermost}
+MATTERMOST_PORT_NUMBER=${MATTERMOST_PORT_NUMBER:-8000}
+NODE_HOST=${NODE_HOST:-node}
+NODE_PORT_NUMBER=${NODE_PORT_NUMBER:-8080}
 
 # Check if SSL should be enabled (if certificates exists)
 if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
@@ -12,13 +14,15 @@ else
   echo "linking plain config"
 fi
 # Linking Nginx configuration file
-ln -s /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
+ln -s /etc/nginx/sites-available/nginx$ssl /etc/nginx/conf.d/nginx.conf
 
 # Setup app host and port on configuration file
-sed -i "s/{%APP_HOST%}/${APP_HOST}/g" /etc/nginx/conf.d/mattermost.conf
-sed -i "s/{%APP_PORT%}/${APP_PORT_NUMBER}/g" /etc/nginx/conf.d/mattermost.conf
+sed -i "s/{%MATTERMOST_HOST%}/${MATTERMOST_HOST}/g" /etc/nginx/conf.d/nginx.conf
+sed -i "s/{%MATTERMOST_PORT%}/${MATTERMOST_PORT_NUMBER}/g" /etc/nginx/conf.d/nginx.conf
+sed -i "s/{%NODE_HOST%}/${NODE_HOST}/g" /etc/nginx/conf.d/nginx.conf
+sed -i "s/{%NODE_PORT%}/${NODE_PORT_NUMBER}/g" /etc/nginx/conf.d/nginx.conf
 
-cat /etc/nginx/conf.d/mattermost.conf
+cat /etc/nginx/conf.d/nginx.conf
 
 # Run Nginx
 exec nginx -g 'daemon off;'
