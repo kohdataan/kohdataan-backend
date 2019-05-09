581734cc791d3d38871ff24537b3fc1544ea6474
Janne Timonen <petrutsi@gmail.com>
2019-05-09 13:50:55 +0300

    cleaned test.http; prettiered users-migration

--- node/database/migrations/20190508153151-Users.js
+++ node/database/migrations/20190508153151-Users.js
@@ -1,49 +1,59 @@
-'use strict';
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('Users', 'nickname',Sequelize.STRING)
+    return queryInterface
+      .addColumn('Users', 'nickname', Sequelize.STRING)
       .then(() => {
         return queryInterface.addColumn('Users', 'location', Sequelize.STRING)
       })
       .then(() => {
-        return queryInterface.addColumn('Users', 'description', Sequelize.STRING)
+        return queryInterface.addColumn(
+          'Users',
+          'description',
+          Sequelize.STRING
+        )
       })
       .then(() => {
         return queryInterface.addColumn('Users', 'profileReady', {
-            allowNull: false,
-            type: Sequelize.BOOLEAN
-          })
+          allowNull: false,
+          type: Sequelize.BOOLEAN,
+        })
       })
       .then(() => {
-        return queryInterface.addColumn('Users','tutorialWatched', {
-            allowNull: false,
-            type: Sequelize.BOOLEAN
-          })
+        return queryInterface.addColumn('Users', 'tutorialWatched', {
+          allowNull: false,
+          type: Sequelize.BOOLEAN,
+        })
       })
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Users', 'nickname',Sequelize.STRING)
-    .then(() => {
-      return queryInterface.removeColumn('Users', 'location', Sequelize.STRING)
-    })
-    .then(() => {
-      return queryInterface.removeColumn('Users', 'description', Sequelize.STRING)
-    })
-    .then(() => {
-      return queryInterface.removeColumn('Users', 'profileReady', {
+    return queryInterface
+      .removeColumn('Users', 'nickname', Sequelize.STRING)
+      .then(() => {
+        return queryInterface.removeColumn(
+          'Users',
+          'location',
+          Sequelize.STRING
+        )
+      })
+      .then(() => {
+        return queryInterface.removeColumn(
+          'Users',
+          'description',
+          Sequelize.STRING
+        )
+      })
+      .then(() => {
+        return queryInterface.removeColumn('Users', 'profileReady', {
           allowNull: false,
-          type: Sequelize.BOOLEAN
+          type: Sequelize.BOOLEAN,
         })
-    })
-    .then(() => {
-      return queryInterface.removeColumn('Users','tutorialWatched', {
+      })
+      .then(() => {
+        return queryInterface.removeColumn('Users', 'tutorialWatched', {
           allowNull: false,
-          type: Sequelize.BOOLEAN
+          type: Sequelize.BOOLEAN,
         })
-    })
-  }
-};
-  
-
+      })
+  },
+}
