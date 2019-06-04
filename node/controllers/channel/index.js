f3683fefdffdbfd23ddd9f4e9443ad910ada28ae
Janne Timonen <petrutsi@gmail.com>
2019-06-04 17:24:27 +0300

    updated axios; endpoint to get channel members by channel id Updated axios package because of a vulnerability in previous version GET channel/members/:id that returns an array of user associated to that channel

--- node/controllers/channel/index.js
+++ node/controllers/channel/index.js
@@ -1,3 +1,8 @@
+import axios from 'axios'
+
+const mattermostUrl =
+  process.env.MATTERMOST_URL || 'http://mattermost:8000/api/v4'
+
 export const sendImageToChannel = (req, res) => {
   res.status(501).send('send image to channel')
 }
@@ -8,4 +13,33 @@ export const sendAudioToChannel = (req, res) => {
 
 export const leaveChannel = (req, res) => {
   res.status(501).send('leave channel')
+}
+
+export const getChannelUsers = async (req, res) => {
+  const { id } = req.params
+  try {
+    axios.defaults.headers.common.Authorization = `Bearer ${
+      process.env.MASTER_TOKEN
+    }`
+    const userIds = await axios.get(`${mattermostUrl}/channels/${id}/members`)
+    const { data } = userIds
+    const ids = data.map(user => user.user_id)
+    const usersResponse = await axios.post(`${mattermostUrl}/users/idss`, ids)
+    const users = usersResponse.data
+    const userDetails = users.map(user => {
+      const { id, username, email, nickname } = user
+      return { id, username, email, nickname }
+    })
+    return res.status(200).send({
+      success: true,
+      message: 'Channel users fetched',
+      userDetails,
+    })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).send({
+      success: false,
+      message: 'Error while fetching channel users',
+    })
+  }
 }
