b8eaf57bef07bfdb60b57c8b94cb0e886db27cca
TerriFin <saukkonensami@gmail.com>
2019-11-29 13:22:04 +0200

    Removed console.logs

--- node/middlewares/checkJwtToken.js
+++ node/middlewares/checkJwtToken.js
@@ -10,21 +10,17 @@ export default (req, res, next) => {
 
   // If call does not have a token, just call next
   if (!jwtToken) {
-    console.log('NO TOKEN IN REQUEST, PROCEEDING....')
     return next()
   }
 
   LogoutToken.findOne({ where: { token: jwtToken } })
     .then(token => {
-      console.log('SOMETHING FOUND')
       // If token is not found, call next as it has not been logged out with
       if (!token) {
-        console.log('TOKEN NOT IN DATABASE, PROCEEDING....')
         return next()
       }
 
       // If token is found, respond with 401 as the token was logged out with (also do not call next())
-      console.log('TOKEN IN DATABASE, ABORTING...')
       return res.status(401).send({
         success: false,
         message: 'You have logged out',
