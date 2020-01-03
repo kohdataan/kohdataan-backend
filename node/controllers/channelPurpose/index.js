a969e84ce1650c478ad71fd35e98a20a6d0cdb5d
TerriFin <saukkonensami@gmail.com>
2020-01-03 07:17:45 +0200

    Optimised channelPurpose controller reset function

--- node/controllers/channelPurpose/index.js
+++ node/controllers/channelPurpose/index.js
@@ -9,10 +9,12 @@ const mattermostUrl =
 
 // this function looks at current users of a channel, and build a purpose based on them
 export const resetChannelPurpose = async (req, res) => {
+  // Set bearer token
   axios.defaults.headers.common.Authorization = `Bearer ${
     process.env.MASTER_TOKEN
   }`
 
+  // Get channel id from request body
   const { channelId } = req.body
 
   try {
@@ -20,46 +22,43 @@ export const resetChannelPurpose = async (req, res) => {
     const usersInChannel = await axios.get(`${mattermostUrl}/users`, {
       in_channel: channelId,
     })
-    // Get a list of promises that each return a user in database
-    const usersPromises = usersInChannel.data.map(mattermostUser =>
-      User.findOne({ where: { email: mattermostUser.email } })
-    )
-    // Wait for the promises to resolve
-    const users = await Promise.all(usersPromises)
-
-    // Get a list of promises that each return a list of users interests
-    const interestsPromises = []
-    users.forEach(user => {
-      // Only run for users that were found in node database
-      if (user) {
-        interestsPromises.push(
-          User.findOne({
-            where: { id: user.id },
-            include: [
-              {
-                model: Interest,
-                as: 'interests',
-                attributes: ['id', 'name'],
-                through: { attributes: [] },
-              },
-            ],
-          }).then(fetchedUsers => {
-            return fetchedUsers.interests
-          })
-        )
-      }
+    // Get a list of promises that each return users interests
+    const usersInterestsPromises = usersInChannel.data.map(mattermostUser => {
+      return User.findOne({
+        where: { email: mattermostUser.email },
+        include: [
+          {
+            model: Interest,
+            as: 'interests',
+            attributes: ['id', 'name'],
+            through: { attributes: [] },
+          },
+        ],
+      })
+        .then(fetchedUsers => {
+          return fetchedUsers.interests
+        })
+        .catch(() => {
+          return undefined
+        })
     })
-    // Wait for the interest promises to resolve
-    const completedInterestPromises = await Promise.all(interestsPromises)
+    // Wait for the promises to resolve
+    const usersInterests = await Promise.all(usersInterestsPromises)
+    // Create empty object we are going to populate with the interests and set to be the purpose of the channel
     const allInterests = {}
 
     // Parse through the resolved promises
-    completedInterestPromises.forEach(completedPromise => {
+    usersInterests.forEach(interests => {
       // Each promise contains a list of interests, add them to the allInterests object
-      completedPromise.forEach(interest => {
-        allInterests[interest.name] =
-          interest.name in allInterests ? (allInterests[interest.name] += 1) : 1
-      })
+      // If user was not found (is undefined), do nothing
+      if (interests) {
+        interests.forEach(interest => {
+          allInterests[interest.name] =
+            interest.name in allInterests
+              ? (allInterests[interest.name] += 1)
+              : 1
+        })
+      }
     })
     // Update mattermost channel purpose to match its users interests
     await axios.put(`${mattermostUrl}/channels/${channelId}/patch`, {
