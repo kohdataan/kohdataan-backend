83af10da5e8f4cbe0db6936cae25ce2a42eaa3e3
TerriFin <saukkonensami@gmail.com>
2020-01-20 00:30:25 +0200

    Fixed starting scripts to work with new volume system, modified emailverification script to use await and added changes required in pull request comments

--- node/controllers/emailverification/index.js
+++ node/controllers/emailverification/index.js
@@ -1,69 +1,69 @@
-import model from '../../models'
+import db from '../../models'
 import sendMail from '../../utils/mailsender'
 
-const { EmailVerificationUuid, User } = model
+const EmailVerificationUuid = db.sequelize.model('EmailVerificationUuid')
+const User = db.sequelize.model('User')
 
-export const handleEmailVerificationRequest = (req, res) => {
+export const handleEmailVerificationRequest = async (req, res) => {
   const { email } = req.body
-  let thisUser = null
 
-  return User.findOne({ where: { email } })
-    .then(user => {
-      thisUser = user
-      return EmailVerificationUuid.findOne({ where: { userId: user.id } })
-    })
-    .then(response => {
-      return response || EmailVerificationUuid.create({ userId: thisUser.id })
-    })
-    .then(createdToken => {
-      const emailToSent = `Hei, tästä linkistä pääset kirjautumaan palveluun: 
-      \nwww.kohdataan.com/verifyemail?uuid=${createdToken.dataValues.uuid}`
-      return sendMail(thisUser.email, 'Kirjautuminen', emailToSent)
-    })
-    .then(response => {
+  try {
+    const user = await User.findOne({ where: { email } })
+    if (user.emailVerified) {
       return res.status(201).send({
         success: true,
-        message: 'Email verificarion link sent',
-        response: response.response,
+        message: 'This account is already verified',
       })
+    }
+    let verificationUuid = await EmailVerificationUuid.findOne({
+      where: { userId: user.id },
     })
-    .catch(err => {
-      return res.status(500).send({
-        success: false,
-        message: 'problem with email verification',
-        error: err,
-      })
+    if (!verificationUuid) {
+      verificationUuid = await EmailVerificationUuid.create({ userId: user.id })
+    }
+    const emailToSent = `Hei, tästä linkistä pääset kirjautumaan palveluun: 
+      \nwww.kohdataan.com/verifyemail/${verificationUuid.uuid}`
+    sendMail(user.email, 'Kirjautuminen', emailToSent)
+    return res.status(201).send({
+      success: true,
+      message: 'Email verification link sent',
     })
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: 'Something went wrong while creating emailVerification token',
+      error: err,
+    })
+  }
 }
 
-export const handleEmailVerification = (req, res) => {
+export const handleEmailVerification = async (req, res) => {
   const { uuid } = req.body
 
-  EmailVerificationUuid.findOne({ where: { uuid } })
-    .then(emailVerificateEntry => {
-      return User.update(
-        {
-          emailVerified: true,
+  try {
+    const verificationUuid = await EmailVerificationUuid.findOne({
+      where: { uuid },
+    })
+    await User.update(
+      {
+        emailVerified: true,
+      },
+      {
+        where: {
+          id: verificationUuid.userId,
         },
-        {
-          where: {
-            id: emailVerificateEntry.dataValues.userId,
-          },
-        }
-      )
+      }
+    )
+    await verificationUuid.destroy()
+    return res.status(200).send({
+      success: true,
+      message: 'User email verified',
     })
-    .then(response => {
-      return res.status(200).send({
-        success: true,
-        message: 'User email verified',
-        response,
-      })
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: 'Given uuid does not match any stored uuids',
+      error: err,
     })
-    .catch(err => {
-      return res.status(500).send({
-        success: false,
-        message: 'Given uuid does not match any stored uuids',
-        error: err,
-      })
-    })
+  }
 }
--- node/routes/emailverification/index.js
+++ node/routes/emailverification/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import * as emailVerification from '../../controllers/emailverification'
+import * as emailVerification from '../../controllers/emailVerification'
 
 const router = express.Router()
 
