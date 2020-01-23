d14ebef3fa4ed16922f5e489f181aee885342523
pninja <pmkuosmanen@gmail.com>
2020-01-23 13:15:14 +0200

    Change script back to how it was, accidentally committed changes.

--- resetProjectDatabase.sh
+++ resetProjectDatabase.sh
@@ -10,12 +10,12 @@ cd db/dumps/
 
 sleep 50
 
-docker exec -i kohdataan-back-marraskuu_db_1 createdb -U mmuser kohdataan
-docker exec -i kohdataan-back-marraskuu_db_1 createdb -U mmuser database_test
-docker exec -i kohdataan-back-marraskuu_db_1 psql -U mmuser kohdataan < kohdataan.pgsql
-docker exec -i kohdataan-back-marraskuu_db_1 psql -U mmuser mattermost < mattermost.pgsql
-docker exec -i kohdataan-back-marraskuu_node_1 sequelize db:migrate
-docker exec -i kohdataan-back-marraskuu_node_1 sequelize db:seed:all
+docker exec -i kohdataan-backend_db_1 createdb -U mmuser kohdataan
+docker exec -i kohdataan-backend_db_1 createdb -U mmuser database_test
+docker exec -i kohdataan-backend_db_1 psql -U mmuser kohdataan < kohdataan.pgsql
+docker exec -i kohdataan-backend_db_1 psql -U mmuser mattermost < mattermost.pgsql
+docker exec -i kohdataan-backend_node_1 sequelize db:migrate
+docker exec -i kohdataan-backend_node_1 sequelize db:seed:all
 
 cd ../../
 
