ac19a062fc0200ddd890350fe0bf4641e9b46882
Laura <laura.reivinen@gmail.com>
2020-11-13 15:07:29 +0200

    removed unnecessary attribute from return

--- node/controllers/channel/index.js
+++ node/controllers/channel/index.js
@@ -27,8 +27,8 @@ export const getChannelUsers = async (req, res) => {
     const usersResponse = await axios.post(`${mattermostUrl}/users/ids`, ids)
     const users = usersResponse.data
     const userDetails = users.map(user => {
-      const { id, username, email, nickname, delete_at, position } = user
-      return { id, username, email, nickname, delete_at, position }
+      const { id, username, nickname, delete_at, position } = user
+      return { id, username, nickname, delete_at, position }
     })
     return res.status(200).send({
       success: true,
